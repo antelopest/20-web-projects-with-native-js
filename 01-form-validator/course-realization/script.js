01eb2fd7849ecb7bf20f5812503163621b6d8925
Simon <antelopest@gmail.com>
2021-04-05 09:12:28 +0300

    01-form-validator: update files

---
+++ 01-form-validator/course-realization/script.js
@@ -0,0 +1,76 @@
+const form = document.getElementById('form');
+
+const username = document.getElementById('username');
+const email = document.getElementById('email')
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+// show input success
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// is valid email
+function checkEmail(input) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+
+    if (re.test(input)) {
+        showSuccess(input);
+    } else {
+        showError(input);
+    }
+
+}
+
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1).toLowerCase();
+}
+
+function checkRequiredField(inputArr) {
+    inputArr.forEach(
+        function(input) {
+            if (input.value.trim() === '') {
+                showError(input, `${getFieldName(input)} is required`);
+            } else {
+                showSuccess(input);
+            }
+        }
+    )
+}
+
+function checkLengthField(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+// event listener
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    checkRequiredField(
+        [email, username, password, password2]
+    );
+
+    checkLengthField(
+        username,
+        3,
+        16
+    );
+});
+
