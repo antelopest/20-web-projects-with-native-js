6930712486e9214cec06b60e6352d5f54453da0d
Simon <antelopest@gmail.com>
2021-04-10 14:45:41 +0300

    01-form-validator: Add func valid email for form

--- 01-form-validator/course-realization/script.js
+++ 01-form-validator/course-realization/script.js
@@ -23,14 +23,11 @@ function showSuccess(input) {
 function checkEmail(input) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-
-
     if (re.test(input)) {
         showSuccess(input);
     } else {
         showError(input);
     }
-
 }
 
 function getFieldName(input) {
